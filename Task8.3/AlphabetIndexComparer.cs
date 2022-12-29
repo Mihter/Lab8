7b0e107d6d3c58ee0615510574d807e75de27710
Kazmin Mihail <mihail.kazmin.04@mail.ru>
2022-12-29 22:11:22 +0500

    fixed program.cs

---
+++ Task8.3/AlphabetIndexComparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace Anagrams
+{
+    class AlphabetIndexComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            char c1 = (char)x;
+            char c2 = (char)y;
+            return c1.CompareTo(c2);
+        }
+
+    }
+
+}
---
+++ Task8.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Anagrams
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] array = new string[] { "code", "doce", "ecod", "framer", "frame" };
+            string[] changedArray = DeleteAnagramsFromArray(array);
+            foreach (string s in changedArray)
+                Console.WriteLine(s);
+
+        }
+
+        private static string[] DeleteAnagramsFromArray(string[] array)
+        {
+            List<string> list = new()
+            {
+                array[0]
+            };
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (!array[i].IsAnagramTo(array[i - 1])) list.Add(array[i]);
+            }
+
+            list.Sort();
+            return list.ToArray();
+        }
+
+    }
+
+}
